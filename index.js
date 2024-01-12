146f36108c1bc14a3f0302f576dfbab29c57ccbb
Apoll0nsLyre <gougeenzo@gmail.com>
2024-01-12 15:18:18 +0100

    improve design, make some adds

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const menuButton = document.querySelector('#menu-button');
 const sideNav = document.querySelector('.Side');
+const taskContainer = document.querySelector('.task-container');
+const modalTriggers = document.querySelectorAll('.modal-trigger');
+const formContainer = document.querySelector('.form-container');
 
 menuButton.addEventListener('click', () => {
     sideNav.classList.toggle('active');
+}
+);
+
+modalTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        formContainer.classList.toggle('active');
+    })
 }
 );
